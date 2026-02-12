9bf241ac2dd8b03e10ee9072f9f34d5e4f8a7c48
codemonk-silver <olajugbagbeeniola12@outlook.com>
2026-02-12 14:17:19 -0800

    service page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
+      <div>
       <main className="relative h-screen w-full">
            <Image
               src="/Background.png"
@@ -10,6 +12,36 @@ export default function Home() {
               priority
               className="object-cover"
             />
+            <div className="flex justify-center relative px-60 top-10">
+                <div className="flex justify-between h-screen items-center">
+                    <div className="basis-[40%] text-black space-y-6">
+                        <h1 className="text-5xl">Build Innovative Apps For Your Business</h1>
+                        <p className="text-[0.85rem] max-w-96">Build smarter, move faster, and grow stronger with custom apps designed to support your business every step of the way.</p>
+                        <Button className="px-8 py-5 bg-amber-600 text-white">Browse Our Services</Button>
+                    </div>
+                    <div className="basis-[45%]">
+                        <Image
+                          src="/images.png"
+                          alt="Background"
+                          width={500}
+                          height={200}
+                          className="object-cover"
+                        />
+                    </div>
+                </div>
+            </div>
       </main>
+
+      {/* servicres */}
+      <div className="relative bg-white w-full h-[50vh] flex justify-center items-center">
+          <Image
+            src="/serviceframe.png"
+            alt="Background"
+            fill
+            className="object-cover shadow-2xl"
+            />
+      </div>
+
+      </div>
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,13 +5,12 @@ import ThemeToggle from "./theme-toggle";
 
 export default function Navbar() {
     return (
-        <div className="w-full h-20 flex justify-between items-center px-36 fixed top-0 left-0 bg-transparent z-10">
+        <div className="w-full h-20 flex justify-between items-center px-60 fixed top-0 left-0 bg-transparent z-10">
             <div className="flex gap-20 items-center">
                 <h1 className="text-3xl font-bold text-black">Candy</h1>
-                <div className="flex gap-5 font-semibold text-black">
+                <div className="flex gap-5 text-black">
                     <Link href="/portfolio">Portfolio</Link>
                     <Link href="/pricing">Pricing</Link>
-                    <Link href="/portfolio">Portfolio</Link>
                     <Link href="/services">Services</Link>
                     <Link href="/blog">Blog</Link>
                     <Link href="/contact">Contact</Link>
